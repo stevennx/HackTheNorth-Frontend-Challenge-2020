48ca9da59a509d401a5791c75e881adc69a44843
StevenXu <steven@property.ca>
2020-02-15 16:12:29 -0500

    Updates

---
+++ precache-manifest.bc53cfd6a5b855e68ae481739468bd66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ced4f0738c705094fc0d591f79d7f1e",
+    "url": "/hackthenorth-frontend-challenge-2020/index.html"
+  },
+  {
+    "revision": "30b81ef2a5475c2844af",
+    "url": "/hackthenorth-frontend-challenge-2020/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c45f8a8a5806af2a7e80",
+    "url": "/hackthenorth-frontend-challenge-2020/static/js/2.5e51fb83.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hackthenorth-frontend-challenge-2020/static/js/2.5e51fb83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b81ef2a5475c2844af",
+    "url": "/hackthenorth-frontend-challenge-2020/static/js/main.64799a4f.chunk.js"
+  },
+  {
+    "revision": "43ca4cb0c03cc47b360a",
+    "url": "/hackthenorth-frontend-challenge-2020/static/js/runtime-main.735ad6ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hackthenorth-frontend-challenge-2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
